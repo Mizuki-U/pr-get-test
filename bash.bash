cde17e5bdcc91b7f567ca8a07059ba690fc8299c
Mizuki-U <85981039+Mizuki-U@users.noreply.github.com>
2021-12-10 14:07:31 +0900

    Add files via upload

---
+++ bash.bash
@@ -0,0 +1,4 @@
+sleep 5s; curl https://review.openstack.org//changes/738483/detail
+sleep 5s; curl https://review.openstack.org//changes/738484/detail
+sleep 5s; curl https://review.openstack.org//changes/738485/detail
+
