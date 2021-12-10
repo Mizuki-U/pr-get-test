3ece9912130290c97508bc3ce68de1d7d98d5273
Houki Kohmoli <32835505+kohmoli@users.noreply.github.com>
2021-12-10 14:09:38 +0900

    add: bash.bash

--- bash.bash
+++ bash.bash
@@ -1,4 +1,3 @@
-sleep 5s; curl https://review.openstack.org//changes/738483/detail
-sleep 5s; curl https://review.openstack.org//changes/738484/detail
-sleep 5s; curl https://review.openstack.org//changes/738485/detail
-
+sleep 10s; curl https://review.openstack.org//changes/738483/detail
+sleep 10s; curl https://review.openstack.org//changes/738484/detail
+sleep 10s; curl https://review.openstack.org//changes/738485/detail
